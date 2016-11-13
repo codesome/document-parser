6ddb36e2a082382fc58e48e543e6bca14b39a5b3
thecodesome <cs15btech11018@iith.ac.in>
2016-11-13 10:40:18 +0530

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # parser
+
+#### Run:
+```
+python3 parser.py
+```
+
+Note: If you want to exclude stop words, check line number `4` and `38` in `parser.py`
+
+#### Dependencies
+* nltk ([Download here](http://www.nltk.org/install.html))
+  * `pos_tag` from `nltk`
+    * If this is not present in default installation of nltk, follow the following procedure to include it
+
+    ``` bash
+    Python 3.5.2 (default, Sep 10 2016, 08:21:44)
+    [GCC 5.4.0 20160609] on linux
+    Type "help", "copyright", "credits" or "license" for more information.
+    >>> import nltk
+    >>> nltk.download()
+    NLTK Downloader
+    ---------------------------------------------------------------------------
+        d) Download   l) List    u) Update   c) Config   h) Help   q) Quit
+    ---------------------------------------------------------------------------
+    Downloader> d
+
+    Download which package (l=list; x=cancel)?
+      Identifier> averaged_perceptron_tagger
+    ```
+
+  * `stopwords` from `nltk.corpus`
+    * If this is not present in default installation of nltk, follow the following procedure to include it
+
+    ``` bash
+    Python 3.5.2 (default, Sep 10 2016, 08:21:44)
+    [GCC 5.4.0 20160609] on linux
+    Type "help", "copyright", "credits" or "license" for more information.
+    >>> import nltk
+    >>> nltk.download()
+    NLTK Downloader
+    ---------------------------------------------------------------------------
+        d) Download   l) List    u) Update   c) Config   h) Help   q) Quit
+    ---------------------------------------------------------------------------
+    Downloader> d
+
+    Download which package (l=list; x=cancel)?
+      Identifier> stopwords
+    ```
---
+++ parser.py
@@ -0,0 +1,98 @@
+import re
+from nltk import pos_tag
+
+### Uncomment these below 2 lines for stopwords ###
+# from nltk.corpus import stopwords
+# stop = set(stopwords.words('english'))
+
+def tokenize(sentence):
+    """ Returns an array with all the words in the sentence """
+    s = sentence[:]
+
+    # comment out the any of the following 4 lines according to the need
+    s = re.compile("\t").sub(" ",s) # replacing tab space with single space
+    s = re.compile("[-_(),&.]").sub(" ",s) # removing special characters - _ ( ) , & .
+    s = re.compile("[^A-Za-z ]").sub("",s) # removing non alphabets
+    s = re.compile("\s\s+").sub(" ",s) # replacing multiple spaces with single space
+
+    s = s.split()
+    return s
+# end tokenize
+
+
+def parse(string):
+    """
+        Returns a dictionary with
+            1. An array of words in the string
+            2. An array of words with their frequencies in descending
+            3. Nouns in the string
+    """
+
+    words = tokenize(string)
+    wordDictionary = {} # words with their frequencies
+
+    for i in range(len(words)):
+        w_lower = words[i].lower()
+
+
+        ### Uncomment these below 2 line to exclude stop words ###
+        #if w_lower in stop:
+        #   continue
+
+        if not w_lower in wordDictionary:
+            wordDictionary[w_lower] = {
+                "word": words[i],
+                "indices": [], # indices of that word in the array 'words'
+                "frequency": 0 # frequency of the word
+            }
+        # end if
+
+        wordDictionary[w_lower]["indices"].append(i)
+        wordDictionary[w_lower]["frequency"] += 1
+    # end for
+
+    reducedArray = []
+    wordTags = []
+    for w in wordDictionary:
+        reducedArray.append(wordDictionary[w])
+        wordTags.append(wordDictionary[w]["word"])
+    # end for
+    del wordDictionary
+
+    wordTags = pos_tag(wordTags)
+    nouns = []
+    for i in range(len(wordTags)):
+        if(wordTags[i][1] == "NN" or wordTags[i][1] == "NNP"):
+            nouns.append(wordTags[i][0])
+        # end if
+    # end for
+    del wordTags
+
+    # sorting in descending
+    reducedArray = sorted(reducedArray , key=lambda wd:wd["frequency"] , reverse=True)
+
+
+    return {
+        "words": words,
+        "wordsWithFrequency": reducedArray, # in descending order
+        "nouns": nouns
+    }
+# end parse
+
+# test sentense
+sentence = "I am a movie fanatic. When friends want to know what picture won the Oscar in 1980 or who played the police chief in Jaws, they ask me. My friends, though, have stopped asking me if I want to go out to the movies. The problems in getting to the theater, the theater itself, and the behavior of some patrons are all reasons why I often wait for a movie to show up on TV."
+
+# parsing the test sentence
+parsedData = parse(sentence)
+freq = parsedData["wordsWithFrequency"]
+nouns = parsedData["nouns"]
+
+# printing the most frequently used words
+m = min(10,len(freq))
+print("Top "+ str(m) + " words")
+for i in range(m):
+    print( "(" + str(freq[i]["frequency"]) + ", \"" + freq[i]["word"] + "\")")
+
+# printing the nouns
+print("\nNouns:")
+print(", ".join(nouns))
